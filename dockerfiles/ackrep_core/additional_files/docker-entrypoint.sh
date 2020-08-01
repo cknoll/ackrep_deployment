5d29ddb77b4ebab3c183ece06a1255ec6e43e735
Carsten Knoll <carsten.knoll@posteo.de>
2020-08-01 13:59:15 +0200

    manually load canonical data (quick fix)

--- dockerfiles/ackrep_core/additional_files/docker-entrypoint.sh
+++ dockerfiles/ackrep_core/additional_files/docker-entrypoint.sh
@@ -6,6 +6,8 @@ if [ "x$DJANGO_MANAGEPY_MIGRATE" = 'xon' ]; then
 
     # create an empty database
     python manage.py migrate --noinput --run-syncdb
+    # ackrep -l ../ackrep_data
+    python -c "from ackrep_core import core; core.load_repo_to_db('/code/ackrep_data')"
 
     # this is the place where fixtures could be loaded
 
