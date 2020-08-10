3d7d9fcc280eeee1dca943e9fb755a88aaac85a4
Carsten Knoll <carsten.knoll@posteo.de>
2020-08-10 11:41:51 +0200

    WIP: reorganize container concept

--- dockerfiles/ackrep_core/additional_files/docker-entrypoint.sh
+++ dockerfiles/ackrep_core/additional_files/docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-
+# this environment variable will be set by docker-compose
 if [ "x$DJANGO_MANAGEPY_MIGRATE" = 'xon' ]; then
 
     # create an empty database
     python manage.py migrate --noinput --run-syncdb
     # ackrep -l ../ackrep_data
-    python -c "from ackrep_core import core; core.load_repo_to_db('/code/ackrep_data')"
+    python -c "from ackrep_core import core; core.load_repo_to_db('$MAIN_DIR/ackrep_data')"
 
     # this is the place where fixtures could be loaded
 
